fcde32d60ee5bae7b6943743085b85c9d7408c32
David <dgrayboff@gmail.com>
2016-05-30 11:53:28 -0400

    js code for switch and started styling alt stylesheet

---
+++ assets/viewmodeswitch.js
@@ -0,0 +1,27 @@
+var frm = document.getElementsByClassName("gridbutton");
+
+for (var i = 0; i < frm.length; ++i) {
+  frm[i].addEventListener("click", function() {
+    var el = document.getElementById("style1");
+    if (el.href.match("assets/styles-list.css")) {
+        el.href = "assets/styles-grid.css";
+     }
+    //  else {
+    //     el.href = "assets/styles-list.css";
+    // }
+  })
+}
+
+var frmTwo = document.getElementsByClassName("listbutton");
+
+for (var i = 0; i < frmTwo.length; ++i) {
+  frmTwo[i].addEventListener("click", function() {
+    var el = document.getElementById("style1");
+    if (el.href.match("assets/styles-grid.css")) {
+        el.href = "assets/styles-list.css";
+     }
+    // else {
+    //  el.href = "assets/styles-list.css";
+    // }
+  })
+}
